0e93ddd2b3b92ec0b490d75b8eacdfb46959cdf3
TainahNeres <20231180012@ifba.edu.br>
2026-02-13 20:53:10 -0300

    Adição de comentários explicativos sobre as rotas e controladores, para melhor compreensão do fluxo de requisições e respostas no sistema.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 class HomeController extends Controller
 {
+/*aqui estamos retornando uma view. no arquivo, a função index é a única existente. então,
+quando estamos na url /, ele vai retornar a view home automaticamente. assim, o fluxo é rota, controller, view*/
     public function index()
     {
         return view('home');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+/*aqui listamos todas as "dependências" das nossas rotas, ou seja, os 
+controladores que serão responsáveis por processar as requisições e retornar as respostas adequadas.*/
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DashboardController;
@@ -8,13 +11,22 @@ use App\Http\Controllers\CampusController;
 use App\Http\Controllers\SetorController;
 use App\Http\Controllers\OrcamentoController;
 
+// Aqui, cada rota possui um controlador responsável, e cada controlador possui métodos 
+// específicos para lidar com as requisições, como "index" para listar recursos
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
 Route::get('/instituicoes', [InstituicaoController::class, 'index'])->name('instituicoes.index');
+
+// aqui temos o método create. no final, a view será um formulário de criação de uma nova instituição.
+// o fluxo é: rota -> controlador -> método create -> view (formulário)
 Route::get('/instituicoes/novo', [InstituicaoController::class, 'create'])->name('instituicoes.create');
+
+// aqui temos o método store. no final, a view será uma redireção para a lista de instituições, 
+// após a criação de uma nova instituição. também tem o botão de adicionar outra instituição, que redireciona 
+// para a view de criação.
 Route::post('/instituicoes', [InstituicaoController::class, 'store'])->name('instituicoes.store');
 
 Route::get('/campi', [CampusController::class, 'index'])->name('campi.index');
